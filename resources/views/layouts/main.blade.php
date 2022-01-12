2d08c7132af11cc6d8ef00d905e035431ba02e97
Paul Klimov <klimov.paul@gmail.com>
2022-01-12 16:50:59 +0200

    fix JS build

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('meta-title') | {{ config('app.name') }}</title>
+    <meta name="description" content="@yield('meta-description', config('app.name'))" />
+    @stack('meta')
+
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Manifest -->
+    <link rel="manifest" href="{{ route('manifest.webmanifest') }}">
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicons/favicon-16x16.png') }}">
+    <link rel="mask-icon" href="{{ asset('img/favicons/safari-pinned-tab.svg') }}" color="#38bdf8">
+    <link rel="shortcut icon" href="{{ asset('img/favicons/favicon.ico') }}">
+
+    <!-- Styles -->
+    @stack('before-styles')
+    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+    @stack('after-styles')
+</head>
+<body class="antialiased text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-900">
+    <div id="app" class="min-h-screen bg-gray-100">
+        @include('includes.header')
+        @yield('content')
+        @include('includes.footer')
+    </div>
+    <!-- Scripts -->
+    @stack('before-scripts')
+    <script src="{{ asset(mix('js/app.js')) }}"></script>
+    @stack('after-scripts')
+</body>
+</html>
